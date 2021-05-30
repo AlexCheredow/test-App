89701d1a001511e13c37278f5bca881577d133e8
AlexCheredow <yasha.cheredow@yandex.ru>
2021-05-30 21:20:21 +0300

    rebuild

--- src/App/App.js
+++ src/App/App.js
@@ -1,18 +1,10 @@
 import React from "react";
 import CurrentlyPlaying from "../components/CurrentlyPlaying";
-// import Bcgrnd from "../img/relax.gif";
 
 import styles from "./App.css";
 function App() {
   return (
-    <div
-      className={styles.div}
-      // style={{
-      //   backgroundImage: "url(" + Bcgrnd + ")",
-      //   width: "100%",
-      //   height: "500px",
-      // }}
-    >
+    <div className={styles.div}>
       <CurrentlyPlaying className={styles.audio} />
     </div>
   );
