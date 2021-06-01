242d44af164be2fa46cf8ff2a2bbac9059d46a22
AlexCheredow <yasha.cheredow@yandex.ru>
2021-06-01 17:17:26 +0300

    add-autoplay-audio-teg

--- src/components/CurrentlyPlaying.js
+++ src/components/CurrentlyPlaying.js
@@ -1,6 +1,10 @@
 const CurrentlyPlaying = () => {
   return (
-    <audio autoPlay controls src="https://air2.radiorecord.ru:9002/lofi_128" />
+    <audio
+      autoPlay="autoplay"
+      controls="controls"
+      src="https://air2.radiorecord.ru:9002/lofi_128"
+    />
   );
 };
 export default CurrentlyPlaying;
